d727c843e27527c858b28bb3837154e80f2ed0ff
CyloTM <samuelchyke@hotmail.com>
2021-03-15 19:48:59 +0000

    Setting up Room Persistence Entities

--- app/src/main/java/com/example/sql_first_try/models/Note.java
+++ app/src/main/java/com/example/sql_first_try/models/Note.java
@@ -3,24 +3,44 @@ package com.example.sql_first_try.models;
 import android.os.Parcel;
 import android.os.Parcelable;
 
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "notes")
 public class Note implements Parcelable {
 
     //Fields
+    //Primary Key
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    @NonNull
+    @ColumnInfo(name = "title")
     private String title;
+
+    @ColumnInfo(name = "content")
     private String content;
+
+    @ColumnInfo(name = "timestamp")
     private String timestamp;
 
     public Note(String title, String content, String timestamp) {
+
         this.title = title;
         this.content = content;
         this.timestamp = timestamp;
     }
 
+    @Ignore
     public Note(){
 
     }
 
     protected Note(Parcel in) {
+        id = in.readInt();
         title = in.readString();
         content = in.readString();
         timestamp = in.readString();
@@ -37,6 +57,14 @@ public class Note implements Parcelable {
             return new Note[size];
         }
     };
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 
     public String getTitle() {
         return title;
@@ -65,11 +93,13 @@ public class Note implements Parcelable {
     @Override
     public String toString() {
         return "Note{" +
+                "id =" + id +
                 "title='" + title + '\'' +
                 ", content='" + content + '\'' +
                 ", timestamp='" + timestamp + '\'' +
                 '}';
     }
+
 
     @Override
     public int describeContents() {
@@ -78,6 +108,7 @@ public class Note implements Parcelable {
 
     @Override
     public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(id);
         dest.writeString(title);
         dest.writeString(content);
         dest.writeString(timestamp);
